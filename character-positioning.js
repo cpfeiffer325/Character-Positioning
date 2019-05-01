4f068f88b11591cc4d6d2508a099f0ee88a37c1d
Christian <c_pfeiffer325@yahoo.ca>
2019-05-01 20:58:35 +0000

    character position function complete and working

--- character-positioning.js
+++ character-positioning.js
@@ -1,19 +1,20 @@
 function countLetters(input) {
-var str = input.join("");
-
 var letterCount = {};
+var str = input.join(" ");
+console.log(str);
 
 for (var i = 0; i < str.length; i++) {
-  if (!letterCount[str[i]]) {
-    letterCount[str[i]] = 1;
-    console.log(str[i]);
-  } else if (letterCount[str[i]]) {
-    letterCount[str[i]]++;
+  if (str[i] !== " ") {
+    if (!letterCount[str[i]]) {
+    letterCount[str[i]] = [i];
+    } else {
+      letterCount[str[i]].push(i);
+    }
   }
 }
 return letterCount;
 }
 
-var myArg = process.argv.slice(2);
+var myArg = process.argv.splice(2);
 
 console.log(countLetters(myArg));
